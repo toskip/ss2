9092e4c30ed26bc6bbd2e77fdb656e3cf35efd98
0sys0 <rain_sys@126.com>
2019-08-11 15:07:07 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install python-pip python-setuptools m2crypto libsodium-dev
+apt-get install -y python-pip python-setuptools m2crypto libsodium-dev
 pip install setuptools --upgrade
 pip install wheel shadowsocks
 ssserver -c shadowsocks.json -d start
