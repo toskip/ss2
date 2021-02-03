7b40d1f5359a916abfcddaefc6a880db1bdfdffa
nemosc <rain_sys@126.com>
2021-02-03 15:21:14 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,4 +1,4 @@
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
-curl -o /usr/local/etc/v2ray/config.json https://github.com/nemosc/ss/blob/master/v2ray_config.json
+curl -o /usr/local/etc/v2ray/config.json https://raw.githubusercontent.com/nemosc/ss/master/v2ray_config.json
 service v2ray stop
 service v2ray start
