8f2aed2731afc96fb7b396dc151acb35502560fc
nemosc <rain_sys@126.com>
2022-03-28 23:33:13 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -4,7 +4,7 @@ curl https://get.acme.sh | sh
 source ~/.bashrc
 ufw allow 80
 ufw allow 443
-~/.acme.sh/acme.sh --register-account -m rain_sys@126.com
+~/.acme.sh/acme.sh --register-account -m rain93sys@gmail.com
 ~/.acme.sh/acme.sh --issue -d nidao.xyz --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d nidao.xyz --fullchainpath /usr/local/etc/v2ray/v2ray.crt --keypath /usr/local/etc/v2ray/v2ray.key --ecc
 curl -o /usr/local/etc/v2ray/config.json https://raw.githubusercontent.com/nemosc/ss/master/v2ray_config.json
