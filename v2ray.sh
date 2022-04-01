fa4060e9779ee00100b2500ce4eb2bb4ffecd185
nemosc <rain_sys@126.com>
2022-04-02 00:36:57 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -8,5 +8,6 @@ ufw allow 443
 ~/.acme.sh/acme.sh --issue -d nidao.xyz --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d nidao.xyz --fullchainpath /usr/local/etc/v2ray/v2ray.crt --keypath /usr/local/etc/v2ray/v2ray.key --ecc
 curl -o /usr/local/etc/v2ray/config.json https://raw.githubusercontent.com/nemosc/ss/master/v2ray_config.json
+chmod 644 /usr/local/etc/v2ray/v2ray.key
 service v2ray stop
 service v2ray start
