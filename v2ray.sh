8f53485508b40c6630069222d0764f53f05d8474
nemosc <rain_sys@126.com>
2022-09-30 18:21:10 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,9 +1,13 @@
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
-apt-get -y install socat
+#apt-get -y install socat
+yum install -y socat
 curl https://get.acme.sh | sh
 source ~/.bashrc
-ufw allow 80
-ufw allow 443
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --permanent --add-port=443/tcp
+firewall-cmd --reload
+#ufw allow 80
+#ufw allow 443
 ~/.acme.sh/acme.sh --register-account -m rain93sys@gmail.com
 ~/.acme.sh/acme.sh --issue -d nidaoo.xyz --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d nidaoo.xyz --fullchainpath /usr/local/etc/v2ray/v2ray.crt --keypath /usr/local/etc/v2ray/v2ray.key --ecc
