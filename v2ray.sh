3fe517e7ed7be1ffc4bad3840b0e0fa9cd84baf6
nemosc <rain_sys@126.com>
2022-10-22 17:00:04 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -4,13 +4,13 @@ yum install -y socat
 curl https://get.acme.sh | sh
 source ~/.bashrc
 firewall-cmd --permanent --add-port=80/tcp
-firewall-cmd --permanent --add-port=443/tcp
+firewall-cmd --permanent --add-port=50443/tcp
 firewall-cmd --reload
 #ufw allow 80
 #ufw allow 443
 ~/.acme.sh/acme.sh --register-account -m rain93sys@gmail.com
-~/.acme.sh/acme.sh --issue -d nidaoo.xyz --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d nidaoo.xyz --fullchainpath /usr/local/etc/v2ray/v2ray.crt --keypath /usr/local/etc/v2ray/v2ray.key --ecc
+~/.acme.sh/acme.sh --issue -d nidao.xyz --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d nidao.xyz --fullchainpath /usr/local/etc/v2ray/v2ray.crt --keypath /usr/local/etc/v2ray/v2ray.key --ecc
 curl -o /usr/local/etc/v2ray/config.json https://raw.githubusercontent.com/nemosc/ss/master/v2ray_config.json
 chmod 644 /usr/local/etc/v2ray/v2ray.key
 service v2ray stop
