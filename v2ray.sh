19cd78421503455fe4d04fd1b9d237ac84f51b68
nemosc <rain_sys@126.com>
2022-12-07 12:40:48 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -8,7 +8,7 @@ firewall-cmd --permanent --add-port=50443/tcp
 firewall-cmd --reload
 #ufw allow 80
 #ufw allow 443
-~/.acme.sh/acme.sh --register-account -m rain93sys@gmail.com
+~/.acme.sh/acme.sh --register-account -m xxx@gmail.com
 ~/.acme.sh/acme.sh --issue -d nidao.xyz --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d nidao.xyz --fullchainpath /usr/local/etc/v2ray/v2ray.crt --keypath /usr/local/etc/v2ray/v2ray.key --ecc
 curl -o /usr/local/etc/v2ray/config.json https://raw.githubusercontent.com/nemosc/ss/master/v2ray_config.json
