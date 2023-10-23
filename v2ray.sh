f98d4d862348773cdb5bee1d5f879e07325f477b
nemosc <rain_sys@126.com>
2023-10-23 16:51:02 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -9,11 +9,11 @@ firewall-cmd --permanent --add-port=60453/tcp
 firewall-cmd --reload
 #ufw allow 80
 #ufw allow 443
-~/.acme.sh/acme.sh --register-account -m xxx@gmail.com
-~/.acme.sh/acme.sh --issue -d nidao.xyz --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d nidao.xyz --fullchainpath /usr/local/etc/v2ray/v2ray.crt --keypath /usr/local/etc/v2ray/v2ray.key --ecc
+#~/.acme.sh/acme.sh --register-account -m xxx@gmail.com
+#~/.acme.sh/acme.sh --issue -d nidao.xyz --standalone -k ec-256
+#~/.acme.sh/acme.sh --installcert -d nidao.xyz --fullchainpath /usr/local/etc/v2ray/v2ray.crt --keypath /usr/local/etc/v2ray/v2ray.key --ecc
 curl -o /usr/local/etc/v2ray/config.json https://raw.githubusercontent.com/nemosc/ss/master/v2ray_config.json
-chmod 644 /usr/local/etc/v2ray/v2ray.key
+#chmod 644 /usr/local/etc/v2ray/v2ray.key
 
 curl -fsSl https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo | tee /etc/yum.repos.d/cloudflare-warp.repo
 yum install -y cloudflare-warp
